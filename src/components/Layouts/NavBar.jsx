a3f03c3a1c0391a5f6135012cf08014667ee3956
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-05-21 00:52:06 -0500

    fix(NavBar):change Title nav

--- src/components/Layouts/NavBar.jsx
+++ src/components/Layouts/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Studio Ghibli :)</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
